5d7d0428bdc43b8d3f722e9de5d12b103daace01
Joseph Hellerstein <jlheller@uw.edu>
2017-10-10 16:18:51 -0700

    Added program to debug

--- Debugging-and-Exceptions/prime.py
+++ Debugging-and-Exceptions/prime.py
@@ -4,33 +4,24 @@
 
 # Prime number finder with a logic bug
 def primeChecker(num):
-  # Inputs: num - number to evaluate as prime
-  # Outputs: is_prime - True/False (is/not a prime)
-  #          explanation - explains why num is not prime
-
-  explanation = ""
-  # prime numbers are greater than 1
-  if num > 1:
-    # check for factors
-    for i in range(2,num):
-      if (num % i) == 0:
-        is_prime = False
-        explanation = "%d times %d is %d" % (i, num//i, num)
-        break
-    else:
-      is_prime = True
-       
-  # if input number is less than
-  # or equal to 1, it is not prime
+  """
+  Checks to see if a number is prime.
+  :param int num:
+  :returns bool:
+  """
+  is_prime = True
+  for i in range(1,num):
+    if (num % i) == 0:
+      is_prime = False
+      break
   else:
-    is_prime = False
-  return (is_prime, explanation)
+    pass
+  return is_prime
 
 # Run the following code if the file is run at the command line
 if __name__ == "__main__":
   num = int(input("Enter a number: "))
-  result = primeChecker(num)
-  if result[0]:
-    print ("%d is prime!" % num)
+  if primeChecker(num):
+    print ("Is prime!")
   else:
-    print ("%d is not prime because %s" % (num, result[1]))
+    print ("Not a prime.")
