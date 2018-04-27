d457f8e48d96e6cffff2c88e424a1fbf523c19f5
Joe Hellerstein <joseph.hellerstein@gmail.com>
2018-04-27 15:15:15 -0700

    Dash lecture.

---
+++ Programming-Interactivity/setup_dash.sh
@@ -0,0 +1,6 @@
+DIR=$HOME/miniconda3/bin
+$DIR/pip install dash==0.21.0  # The core dash backend
+$DIR/pip install dash-renderer==0.12.1  # The dash front-end
+$DIR/pip install dash-html-components==0.10.0  # HTML components
+$DIR/pip install dash-core-components==0.22.1  # Supercharged components
+$DIR/pip install plotly --upgrade  # Latest Plotly graphing library
