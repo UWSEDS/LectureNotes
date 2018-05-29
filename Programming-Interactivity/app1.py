f38c6adf1b7c7a8ae166883bf1e00d1248e2e5b5
Joe Hellerstein <joseph.hellerstein@gmail.com>
2018-05-29 13:42:57 -0700

    Added lecture material

--- Programming-Interactivity/app1.py
+++ Programming-Interactivity/app1.py
@@ -17,7 +17,7 @@ app.layout = html.Div(children=[
         id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [4, 1, 'a'], 'type': 'bar', 'name': 'SF'},
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': 'Montreal'},
             ],
             'layout': {
--- Programming-Interactivity/app4.py
+++ Programming-Interactivity/app4.py
@@ -8,17 +8,20 @@ from dash.dependencies import Input, Output
 app = dash.Dash()
 
 app.layout = html.Div([
-    dcc.Input(id='my-id', value='initial value', type='text'),
+    dcc.Input(id='my-id1', value='initial value1', type='text'),
+    dcc.Input(id='my-id2', value='initial value2', type='text'),
     html.Div(id='my-div'),
 ])
 
 
 @app.callback(
     Output(component_id='my-div', component_property='children'),
-    [Input(component_id='my-id', component_property='value')]
+    [Input(component_id='my-id1', component_property='value'),
+     Input(component_id='my-id2', component_property='value')]
 )
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+def update_output_div(input_value1, input_value2):
+    print(input_value1, input_value2)
+    return 'You\'ve entered "{}"'.format(input_value1 + input_value2)
 
 
 if __name__ == '__main__':
