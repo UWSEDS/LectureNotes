3a0dffbbc166ad11c75fca99aa5fd16b715f8cf1
Joe Hellerstein <joseph.hellerstein@gmail.com>
2018-10-25 13:23:13 -0700

    Updated lecture notes

--- 09_UnitTests/test_prime.py
+++ 09_UnitTests/test_prime.py
@@ -2,14 +2,8 @@ import unittest
 from prime import check_prime
 
 # Define a class in which the tests will run
-class PrimeTest(unittest.TestCase):
+# Insert code like that used in notebook
 
-    def test_smoke(self):
-        check_prime(3)
 
-    def testSimple(self):
-        self.assertTrue(check_prime(3))
-    
-    
 if __name__ == '__main__':
     unittest.main()
