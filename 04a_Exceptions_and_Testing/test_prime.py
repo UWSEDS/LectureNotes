0719ab292c25af75f8cd94dcfd36f57c671dc2d1
Joe Hellerstein <joseph.hellerstein@gmail.com>
2019-04-23 20:01:24 -0700

    Updated files from class

--- 04a_Exceptions_and_Testing/test_prime.py
+++ 04a_Exceptions_and_Testing/test_prime.py
@@ -1,14 +1,9 @@
-import unittest
 from prime import check_prime
 
-# Define a class in which the tests will run
-# Insert code like that used in notebook
+# Should be prime
+def test_oneshot1():
+  assert(check_prime(3))
 
-class UnitTests(unittest.TestCase):
-
-    # Each method in the class to execute a test
-    def test_success(self):
-        self.assertTrue(check_prime(5))
-
-if __name__ == '__main__':
-    unittest.main()
+# Should not be prime
+def test_oneshot2():
+  assert(not check_prime(2))
