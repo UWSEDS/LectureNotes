b2a5e63febbaf45051cbc9466a0766c08397a7e3
Joe Hellerstein <joseph.hellerstein@gmail.com>
2019-05-07 14:25:24 -0700

    Updated tests

--- 04a_Exceptions_and_Testing/test_prime.py
+++ 04a_Exceptions_and_Testing/test_prime.py
@@ -7,3 +7,11 @@ def test_oneshot1():
 # Should not be prime
 def test_oneshot2():
   assert(not check_prime(2))
+
+# check for exception
+def test_exception():
+  try:
+    check_prime(-2)
+    assert(False)
+  except ValueError:
+    assert(True)
