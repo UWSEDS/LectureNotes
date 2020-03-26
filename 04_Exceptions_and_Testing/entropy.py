65ce3f9b16196bedde5a337fdfd5f897d5e5a5b7
Joseph Hellerstein <jlheller@uw.edu>
2020-03-26 09:53:22 -0700

    Setup lecture notes for Spring 2020

--- 04_Exceptions_and_Testing/entropy.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def entropy(ps):
-    check1 = (0 <= sum(ps)) and (1 >= sum(ps))
-    trues = [(0 <= v) and (1 >= v) for v in ps]
-    if not( check1 and all(trues)):
-        raise ValueError("Invalid input.")
-    return -sum(ps * np.log(ps))
